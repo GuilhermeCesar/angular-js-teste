ea616c5104034f765b52a127d6f8ce3c4063e3a1
Guilherme César Medeiros <guilherme.medeiros@terceiros.calcard.com.br>
2019-02-26 09:01:08 -0300

    Criação de tela que abre o modal

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,27 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$uibModal','$scope',function($uibModal,$scope) {
+    $scope.openModal = (size, parentSelectior) => {
+        let modalInstance = $uibModal.open({
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceController',
+            controllerAs: '$ctrl',
+            size: size,
+            // appendTo: parentElem,
+            // resolve: {
+            //     items: function () {
+            //         return $ctrl.items;
+            //     }
+            // }
+        });
+    };
+}])
+.controller('ModalController', ['$uibModal','$scope',function($uibModal,$scope) {
 
-}]);
+}])
+.controller('ModalInstanceController', ['$uibModalInstance','itens',function ($uibModalInstance,itens) {
+
+}])
